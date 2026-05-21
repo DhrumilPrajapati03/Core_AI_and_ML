ace4833171d058f137ade36a97b08674701021bd
Dhrumil Prajapati <dhrumil.prajapati@clicode.co>
2026-05-21 17:04:43 +0530

    Series

--- Pandas/series.py
+++ Pandas/series.py
@@ -1,3 +1,16 @@
 #syntax
 # import pandas as pd
 # s = pd.Series(data,index,dtype,copy=False)
+
+import pandas as pd
+s = pd.Series([1, 3, 5, 7, 9, 11])
+print(s)
+
+print("S[0] = ", s[0])
+b = s[0] + s[1]
+print("Sum = ", b)
+
+k = pd.Series([1, 3, 5, 7, 9, 11], dtype=str)
+print("S[0] = ", s[0])
+b = k[0] + k[1]
+print("Sum = ", b)
