c76b2eeae20782ca9f02e3075cdc7af427b45373
Dhrumil Prajapati <dhrumil.prajapati@clicode.co>
2026-05-22 17:19:33 +0530

    Add initial dataframe creation and basic operations

---
+++ Pandas/dataframe.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+
+arr = np.random.randint(1,100,20).reshape(5,4)
+df = pd.DataFrame(arr,np.arange(101,106,1),['maths','science', 'bio', 'programming'])
+print(df)
+print(type(df))
+print(df.ndim)
+
+# print(df['bio'])
+# print(df[['bio','science']])
+
+# print(df.loc[101])
+
+# print(df.iloc[2])
+
+# df.drop(101,inplace=True)
+# print(df)
+
+# df.drop('bio', axis=1, inplace=True)
+# print(df)
+
+print(df.loc[[101,102], ['maths','science']])
+
+# df['total'] = df['maths'] + df['science'] + df['bio'] + df['programming']
+# print(df)
